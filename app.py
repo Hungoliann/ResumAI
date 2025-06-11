5347377c968f8a58e041c1c1ab200a134b3273d3
Hungoliann <hungle2805@gmail.com>
2025-06-11 14:17:05 -0700

    fixed possible bugs
    remove while loop as streamlit already took care of the looping

--- app.py
+++ app.py
@@ -14,16 +14,16 @@ st.markdown("""
 - Paste a job description  
 - Get a match score and keyword feedback
 """)
-
+if not os.path.exists("assets"): 
+    os.makedirs("assets")
+    
 uploaded_file = st.file_uploader("Upload your resume", type= ["pdf", "txt", "docx"])
+
+    
 with st.spinner("Running your resume through our system..."):
-    while uploaded_file: 
+    if uploaded_file: 
         # Step: Save it temporarily so parse_resume() can read it
-        if os.path.exists("assets"):
-            with open(r"assets\temp_resume", "wb") as resume:
+            with os.path.join("assets\temp_resume", "wb") as resume:
                 # get bytes from the streamlit upload
                 resume.write(uploaded_file.getbuffer())
-                parsed_text = parse_resume(resume)
-                break
-        else:
-            os.makedirs("ResumAI/assets")
+                parsed_text = parse_resume(r"assets\temp_resume")
