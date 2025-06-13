aec974f8a436ff1699f5791aa804ddd20631a7de
Hungoliann <hungle2805@gmail.com>
2025-06-13 05:15:32 -0700

    added stopword filtering
    Added stopword filtering using list of stopwords

--- utils/match_resume_to_job.py
+++ utils/match_resume_to_job.py
@@ -12,6 +12,22 @@ Args:
 Returns:
     tuple: (match_score as float, list of missing keywords)
 """
+stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 
+'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 
+'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 
+'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves',
+ 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are',
+ 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing',
+ 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 
+'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 
+'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 
+'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 
+'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 
+'s', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y',
+ 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 
+'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn',
+ "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 
+'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
 def match_resume_to_job(resume_text: str, job_desc_text: str) -> tuple[float, list[str]]:
 
@@ -23,9 +39,13 @@ def match_resume_to_job(resume_text: str, job_desc_text: str) -> tuple[float, li
     resume_tokens = resume_text.split()
     job_tokens = job_desc_text.split()
 
+    # Filter stopwords from texts 
+    resume_filtered = [w for w in resume_tokens if w not in stop_words]
+    job_filtered = [w for w in job_tokens if w not in stop_words]
+
     # Convert token lists to sets for faster membership testing and unique words
-    resume_set = set(resume_tokens)
-    job_set = set(job_tokens)
+    resume_set = set(resume_filtered)
+    job_set = set(job_filtered)
 
     # Find common words between resume and job description
     common_words = resume_set.intersection(job_set)
